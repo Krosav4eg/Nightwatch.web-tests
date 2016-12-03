3bc82ad0568aa272726749cab1a56fb08011bb64
Sergey_Potapov <s.potapov@andersenlab.com>
2016-12-03 20:48:07 +0200

    eliminating the dependencies of the tests from each other

--- e2e/Agenda/Rooms/Element requiring table setting/Lunch Placeholder.e2e.js
+++ e2e/Agenda/Rooms/Element requiring table setting/Lunch Placeholder.e2e.js
@@ -10,7 +10,30 @@ module.exports = _.assign(presteps, auth, {
             .waitForElementNotVisible('#thisIsMainLoader', 10000)
             .pause(1000);
     },
-
+    'creation new container': function (browser) {
+        browser
+            .waitForElementVisible('button.btn.btn-primary.btn-block', 10000)
+            .click('button.btn.btn-primary.btn-block')
+            .pause(2000)
+            .useXpath()
+            .assert.containsText('//h4[contains(text(),"Container form")]', 'Container form')
+            .useCss()
+            .waitForElementVisible('input#heading', 1000)
+            .setValue('input#heading', 'new_event2016 ')
+            .waitForElementVisible('input#subHeading', 1000)
+            .setValue('input#subHeading', 'test')
+            .waitForElementVisible('#containerStartHour input', 1000)
+            .setValue('#containerStartHour input', '8:00')
+            .waitForElementVisible('#containerEndHour input', 1000)
+            .setValue('#containerEndHour input', '10:00')
+            .useXpath()
+            .click('//form/div[2]/div/div/button[contains(text(),"Save")]')
+            .pause(3000)
+            .assert.elementPresent('//b[contains(text(), "new_event2016")]')
+            .assert.containsText('//b[1][contains(text(),"8:00")]', '8:00')
+            .assert.containsText('//b[2][contains(text(),"10:00")]', '10:00')
+            .pause(2000);
+    },
     'click on add element button': function (browser) {
         browser
             .pause(2000)
@@ -207,5 +230,19 @@ module.exports = _.assign(presteps, auth, {
             .assert.elementNotPresent('//h5[contains(text(),"08:00 - 09:45")]')
             .assert.elementNotPresent('//h5[contains(text(),"Lunch Placeholder")]')
             .assert.elementNotPresent('//button[contains(text(), "       MyRoom     ")]');
+    },
+    'delete container': function (browser) {
+        browser
+            .useCss()
+            .click('.fa.fa-trash-o.delete-container')
+            .pause(1000)
+            .waitForElementVisible('div.modal-footer>button.btn.btn-success', 1000)
+            .click('div.modal-footer>button.btn.btn-success')
+            .pause(3000)
+            .waitForElementNotVisible('#thisIsMainLoader', 10000)
+            .pause(1000)
+            .useXpath()
+            .assert.elementNotPresent('//b[contains(text(), "new_event2016")]')
+            .pause(1000);
     },
 });
