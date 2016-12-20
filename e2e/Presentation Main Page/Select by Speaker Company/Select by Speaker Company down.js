b5f67dba2fc67cde2e1110c3e85746f9c0e3eac7
Serhii Potapov <serhii_potapov@epam.com>
2016-12-20 23:20:31 +0200

    auto test for Select by Speaker Company

--- e2e/Presentation Main Page/Select by Speaker Company/Select by Speaker Company down.js
+++ e2e/Presentation Main Page/Select by Speaker Company/Select by Speaker Company down.js
@@ -10,25 +10,26 @@ module.exports = _.assign(presteps, auth, {
             .waitForElementVisible('#thisIsMainLoader', 30000)
             .waitForElementNotVisible('#thisIsMainLoader', 30000);
     },
-    'select by presentation headings down': function (browser) {
+    'select by speaker company up': function (browser) {
         browser
             .useXpath()
-            .waitForElementVisible('//tr/th[5]', 10000)
-            .click('//tr/th[5]')
+            .waitForElementVisible('//tr/th[7]', 10000)
+            .click('//tr/th[7]')
             .useCss()
             .waitForElementVisible('#thisIsMainLoader', 30000)
             .waitForElementNotVisible('#thisIsMainLoader', 30000)
             .useXpath()
-            .waitForElementVisible('//tr/th[5]', 10000)
-            .click('//tr/th[5]')
+            .waitForElementVisible('//tr/th[7]', 10000)
+            .click('//tr/th[7]')
             .useCss()
             .waitForElementVisible('#thisIsMainLoader', 30000)
             .waitForElementNotVisible('#thisIsMainLoader', 30000)
             .useXpath()
-            .waitForElementVisible('//tr[1]/td[5]/span/div[text()="           Some heading           "]',5000)
-            .assert.elementPresent('//tr[1]/td[5]/span/div[text()="           Some heading           "]')
-            .assert.elementPresent('//tr[2]/td[5]/span/div[text()="           New Heading           "]');
-
+            .waitForElementVisible('//tr[1]/td[7]/span/ul/li[text()="             Telenor ASA           "]', 5000)
+            .assert.elementPresent('//tr[1]/td[7]/span/ul/li[text()="             Telenor ASA           "]')
+            .assert.elementPresent('//tr[2]/td[7]/span/ul/li[text()="             EWE AG           "]')
+            .assert.elementPresent('//tr[2]/td[7]/span/ul/li[2][text()="             SpareBank 1 Gruppen AS           "]')
+            
     },
 
 
