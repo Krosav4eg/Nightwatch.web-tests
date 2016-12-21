8528cc8c21fe210e9eab6c937d000b588e7642da
Serhii Potapov <serhii_potapov@epam.com>
2016-12-21 19:17:53 +0200

    autotests for search by created by. 3 letter  has been created

--- e2e/Presentation Main Page/Search by Speaker company/search by created by. 3 letter.js
+++ e2e/Presentation Main Page/Search by Speaker company/search by created by. 3 letter.js
@@ -3,33 +3,26 @@ var presteps = require('./../../presteps/presteps.js');
 var auth = require('./../../presteps/auth.js');
 
 module.exports = _.assign(presteps, auth, {
-    '@disabled': false,
+    '@disabled': true,
     'redirection to agenda': function (browser) {
         browser
             .relUrl('/event/212/presentations')
             .waitForElementVisible('#thisIsMainLoader', 30000)
             .waitForElementNotVisible('#thisIsMainLoader', 30000);
     },
-    'select by speaker company up': function (browser) {
+    'search by 3 letters': function (browser) {
         browser
-            .useXpath()
-            .waitForElementVisible('//tr/th[7]', 10000)
-            .click('//tr/th[7]')
-            .useCss()
-            .waitForElementVisible('#thisIsMainLoader', 30000)
-            .waitForElementNotVisible('#thisIsMainLoader', 30000)
-            .useXpath()
-            .waitForElementVisible('//tr/th[7]', 10000)
-            .click('//tr/th[7]')
-            .useCss()
-            .waitForElementVisible('#thisIsMainLoader', 30000)
-            .waitForElementNotVisible('#thisIsMainLoader', 30000)
-            .useXpath()
-            .waitForElementVisible('//tr[1]/td[7]/span/ul/li[text()="             Telenor ASA           "]', 5000)
-            .assert.elementPresent('//tr[1]/td[7]/span/ul/li[text()="             Telenor ASA           "]')
-            .assert.elementPresent('//tr[2]/td[7]/span/ul/li[text()="             EWE AG           "]')
-            .assert.elementPresent('//tr[2]/td[7]/span/ul/li[2][text()="             SpareBank 1 Gruppen AS           "]')
+        .useXpath()
+        .waitForElementVisible('//tr/td[7]/input[@type="text"]', 10000)
+        .setValue('//tr/td[7]/input[@type="text"]', ['tel', browser.Keys.ENTER])
+        .useCss()
+        .waitForElementVisible('#thisIsMainLoader', 30000)
+        .waitForElementNotVisible('#thisIsMainLoader', 30000)
+        .useXpath()
+        .waitForElementVisible('//tr[1]/td[7]/span/ul/li[text()="             Telenor ASA           "]', 5000)
+        .assert.elementPresent('//tr[1]/td[7]/span/ul/li[text()="             Telenor ASA           "]')
 
+           
     },
 
 
