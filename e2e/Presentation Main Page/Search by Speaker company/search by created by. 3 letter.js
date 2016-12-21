7fe01095ef2076aea80609609167995290f1c4f6
Serhii Potapov <serhii_potapov@epam.com>
2016-12-21 16:55:26 +0200

    select press on organizer name method  has been changed

--- e2e/Presentation Main Page/Search by Speaker company/search by created by. 3 letter.js
+++ e2e/Presentation Main Page/Search by Speaker company/search by created by. 3 letter.js
@@ -2,38 +2,33 @@ var _ = require('lodash');
 var presteps = require('./../../presteps/presteps.js');
 var auth = require('./../../presteps/auth.js');
 
-
 module.exports = _.assign(presteps, auth, {
-    '@disabled': true,
+    '@disabled': false,
     'redirection to agenda': function (browser) {
         browser
             .relUrl('/event/212/presentations')
             .waitForElementVisible('#thisIsMainLoader', 30000)
             .waitForElementNotVisible('#thisIsMainLoader', 30000);
     },
-    'select press on organizer name': function (browser) {
+    'select by speaker company up': function (browser) {
         browser
             .useXpath()
-            .waitForElementVisible('//tr/th[3]', 10000)
-            .click('//tr/th[3]')
+            .waitForElementVisible('//tr/th[7]', 10000)
+            .click('//tr/th[7]')
             .useCss()
             .waitForElementVisible('#thisIsMainLoader', 30000)
             .waitForElementNotVisible('#thisIsMainLoader', 30000)
             .useXpath()
-            .click('//tr/th[3]')
+            .waitForElementVisible('//tr/th[7]', 10000)
+            .click('//tr/th[7]')
             .useCss()
             .waitForElementVisible('#thisIsMainLoader', 30000)
             .waitForElementNotVisible('#thisIsMainLoader', 30000)
             .useXpath()
-            .waitForElementVisible('//tr[1]/td[3]/span/a[text()="           VTT Technical Research Centre of Finland          "]', 5000)
-            .click('//tr[1]/td[3]/span/a[text()="           VTT Technical Research Centre of Finland          "]')
-            .useCss()
-            .waitForElementVisible('#thisIsMainLoader', 30000)
-            .waitForElementNotVisible('#thisIsMainLoader', 30000)
-            .useXpath()
-            .waitForElementVisible('//div[text()="           Participation info - VTT Technical Research Centre of Finland  - (E#7589)           "]', 10000)
-
-
+            .waitForElementVisible('//tr[1]/td[7]/span/ul/li[text()="             Telenor ASA           "]', 5000)
+            .assert.elementPresent('//tr[1]/td[7]/span/ul/li[text()="             Telenor ASA           "]')
+            .assert.elementPresent('//tr[2]/td[7]/span/ul/li[text()="             EWE AG           "]')
+            .assert.elementPresent('//tr[2]/td[7]/span/ul/li[2][text()="             SpareBank 1 Gruppen AS           "]')
 
     },
 
