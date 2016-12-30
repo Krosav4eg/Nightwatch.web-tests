750f72e1eb8776aec11d01cf20bef8b32ccbbf81
Sergey_Potapov <s.potapov@andersenlab.com>
2016-12-30 09:06:34 +0200

    autotest action  for presentation page have been created and autotest for Event Roles of selects by event role list have been changed

---
+++ e2e/Event Roles/Main Page/search by personal paramets/search by created by.js
@@ -0,0 +1,29 @@
+// var _ = require('lodash');
+// var presteps = require('./../../../presteps/presteps.js');
+// var auth = require('./../../../presteps/auth.js');
+//
+// module.exports = _.assign(presteps, auth, {
+//     '@disabled': false,
+//     'redirection to agenda': function (browser) {
+//         browser
+//             .relUrl('/event/212/event-participant-roles')
+//             .waitForElementVisible('#thisIsMainLoader', 30000)
+//             .waitForElementNotVisible('#thisIsMainLoader', 30000);
+//     },
+//     'search by function title ': function (browser) {
+//         browser
+//             .useXpath()
+//             .waitForElementVisible('//h1[text()="Event role"]', 3000)
+//             .waitForElementVisible('//tr[1]/td[10]/input[@type="text"]', 3000)
+//             .setValue('//tr[1]/td[10]/input[@type="text"]', ['Xsolve Test User', browser.Keys.ENTER])
+//             .useCss()
+//             .waitForElementVisible('#thisIsMainLoader', 3000)
+//             .waitForElementNotVisible('#thisIsMainLoader', 3000)
+//             .useXpath()
+//             .waitForElementVisible('//span[text()="       Xsolve Test User     "]', 3000)
+//
+//     },
+//
+//
+//
+// });
---
+++ e2e/Event Roles/Main Page/search by personal paramets/search by last name.js
@@ -0,0 +1,56 @@
+var _ = require('lodash');
+var presteps = require('./../../../presteps/presteps.js');
+var auth = require('./../../../presteps/auth.js');
+
+module.exports = _.assign(presteps, auth, {
+    '@disabled': true,
+    'redirection to agenda': function (browser) {
+        browser
+            .relUrl('/event/212/event-participant-roles')
+            .waitForElementVisible('#thisIsMainLoader', 30000)
+            .waitForElementNotVisible('#thisIsMainLoader', 30000);
+    },
+    'search by last full name ': function (browser) {
+        browser
+            .useXpath()
+            .waitForElementVisible('//h1[text()="Event role"]', 3000)
+            .waitForElementVisible('//tr[1]/td[4]/input[@type="text"]', 3000)
+            .setValue('//tr[1]/td[4]/input[@type="text"]', ['Alphéus', browser.Keys.ENTER])
+            .useCss()
+            .waitForElementVisible('#thisIsMainLoader', 3000)
+            .waitForElementNotVisible('#thisIsMainLoader', 3000)
+            .useXpath()
+            .waitForElementVisible('//a[contains(text(), "Alphéus")]', 3000)
+
+    },
+    'search by 3 letters last name ': function (browser) {
+        browser
+            .useXpath()
+            .waitForElementVisible('//h1[text()="Event role"]', 3000)
+            .waitForElementVisible('//tr[1]/td[4]/input[@type="text"]', 3000)
+            .clearValue('//tr[1]/td[4]/input[@type="text"]')
+            .setValue('//tr[1]/td[4]/input[@type="text"]', ['Alp', browser.Keys.ENTER])
+            .useCss()
+            .waitForElementVisible('#thisIsMainLoader', 3000)
+            .waitForElementNotVisible('#thisIsMainLoader', 3000)
+            .useXpath()
+            .waitForElementVisible('//a[contains(text(), "Alphéus")]', 3000)
+
+    },
+    'search by 1 letters last name ': function (browser) {
+        browser
+            .useXpath()
+            .waitForElementVisible('//h1[text()="Event role"]', 3000)
+            .waitForElementVisible('//tr[1]/td[4]/input[@type="text"]', 3000)
+            .clearValue('//tr[1]/td[4]/input[@type="text"]')
+            .setValue('//tr[1]/td[4]/input[@type="text"]', ['A', browser.Keys.ENTER])
+            .useCss()
+            .waitForElementVisible('#thisIsMainLoader', 3000)
+            .waitForElementNotVisible('#thisIsMainLoader', 3000)
+            .useXpath()
+            .waitForElementVisible('//a[contains(text(), "Alphéus")]', 3000)
+
+    },
+
+
+});
