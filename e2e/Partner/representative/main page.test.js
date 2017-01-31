241b209804f16a7b47c1ffaadaf558eef4b3da44
TanyaAndreeva <t.andreeva@andersenlab.com>
2017-01-31 11:03:43 +0300

    refactoring

---
+++ e2e/Partner/representative/main page.test.js
@@ -0,0 +1,23 @@
+var _ = require('lodash');
+var presteps = require('./../../presteps/presteps.js');
+var auth = require('./../../presteps/auth.js');
+
+module.exports = _.assign(presteps, auth, {
+
+    'redirection to representatives': function (browser) {
+        browser
+            .relUrl('/event/2/representatives')
+            .waitForElementVisible('#thisIsMainLoader', 30000)
+            .waitForElementNotVisible('#thisIsMainLoader', 30000);
+    },
+
+    'check event data': function (browser) {
+        browser
+            .useXpath()
+            .waitForElementVisible('//h4[text()="Event (#2)"]', 3000)
+            .verify.elementPresent('//h3[text()="Growth Session Dubai Breakfast March 2015"]')
+            .verify.elementPresent('//div[text()="Local name: Growth Session Dubai Breakfast March 2015"]')
+            .verify.elementPresent('//div[text()="Dates: 2015-03-04 08:00:00 - 2015-03-04 18:00:00"]')
+            .verify.elementPresent('//div[text()="Venue: Le Royal Méridien Beach Resort & Spa, Dubai"]');
+    },
+});
