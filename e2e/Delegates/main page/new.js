3e255bd60e00ff06d3dbf60ad0c8a2445ea509c3
TanyaAndreeva <t.andreeva@andersenlab.com>
2017-01-31 12:03:26 +0300

    add

--- e2e/Delegates/main page/new.js
+++ e2e/Delegates/main page/new.js
@@ -1,3 +1,13 @@
-/**
- * Created by andersen on 31.01.2017.
- */
+var _ = require('lodash');
+var presteps = require('./../../presteps/presteps.js');
+var auth = require('./../../presteps/auth.js');
+
+module.exports = _.assign(presteps, auth, {
+
+    'redirection to agenda': function (browser) {
+        browser
+            .relUrl('/event/212/delegates')
+            .waitForElementVisible('#thisIsMainLoader', 30000)
+            .waitForElementNotVisible('#thisIsMainLoader', 30000);
+    },
+});
