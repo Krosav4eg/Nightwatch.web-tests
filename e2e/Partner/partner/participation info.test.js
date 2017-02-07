5d2a258217b9028c74fe23b9ec41b81a911ee48c
TanyaAndreeva <t.andreeva@andersenlab.com>
2017-02-07 17:10:58 +0300

    add tests

--- e2e/Partner/partner/participation info.test.js
+++ e2e/Partner/partner/participation info.test.js
@@ -13,51 +13,63 @@ module.exports = _.assign(presteps, auth, {
 
     'verify Participation info': function (browser) {
         browser
+            .useXpath()
             .verify.containsText('(//h4/a/div)[1]', "Participation info - 4power Infocom Free Zone Company LLC - (E#25)")
 
             .clickBySelectorXpath('(//h4/a/div)[1]')
-            .verify.elementNotPresent('//label[text()="Master company name:"]')
-            .clickBySelectorXpath('(//h4/a/div)[1]')
-            .verify.elementPresent('//label[text()="Master company name:"]');
+            .expect.element('//label[text()="Master company name:"]').to.not.be.visible;
+
+        browser.clickBySelectorXpath('(//h4/a/div)[1]')
+            .expect.element('//label[text()="Master company name:"]').to.be.visible;
+
     },
 
     'verify Master company name': function (browser) {
         browser
-            .verify.containsText('//label[text()="Master company name:"]/../div', "4power Infocom Free Zone Company LLC")
+            .useXpath()
+            .verify.containsText('//*[text()="Master company name:"]/../div', "4power Infocom Free Zone Company LLC")
 
-            .clickBySelectorXpath('//label[text()="Master company name:"]/../div/a')
+            .click('//label[text()="Master company name:"]/../div/a')
+            .pause(1000)
 
             .verify.urlContains("/EW/MasterCompany/cruII/id/")
             .back()
-            .waitForElementNotVisible('#thisIsMainLoader', 30000)
     },
 
-    'verify Participation name': function (browser) {
+  'verify Participation name': function (browser) {
         browser
-            .verify.containsText('//label[text()="Participation name:"]/../div/input', "4power Infocom Free Zone Company LLC")
+            .waitForElementVisible('//label[text()="Participation name:"]/../div/input', 30000)
+            .verify.valueContains('//label[text()="Participation name:"]/../div/input', "4power Infocom Free Zone Company LLC")
     },
 
     'verify Participation status': function (browser) {
         browser
-            .clickBySelectorXpath('(//option[@value=2])[2]')
+            .clickBySelectorXpath('(//option[@value=2])[1]')
             .verify.containsText('//label[text()="Participation status:"]/../div/select', "Cancelled")
 
-            .clickBySelectorXpath('(//option[@value=2])[3]')
+            .clickBySelectorXpath('(//option[@value=3])[1]')
             .verify.containsText('//label[text()="Participation status:"]/../div/select', "Unconfirmed (rebook)")
 
-            .clickBySelectorXpath('(//option[@value=2])[4]')
+            .clickBySelectorXpath('(//option[@value=4])[1]')
             .verify.containsText('//label[text()="Participation status:"]/../div/select', "Confirmed (rebook)")
 
-            .clickBySelectorXpath('(//option[@value=2])[0]')
+            .clickBySelectorXpath('(//option[@value=0])[1]')
             .verify.containsText('//label[text()="Participation status:"]/../div/select', "Unconfirmed")
 
-            .clickBySelectorXpath('(//option[@value=2])[1]')
+            .clickBySelectorXpath('(//option[@value=1])[1]')
             .verify.containsText('//label[text()="Participation status:"]/../div/select', "Confirmed")
 
-            .clickBySelectorXpath('(//option[@value=2])[2]')
+            .clickBySelectorXpath('(//option[@value=2])[1]')
     },
 
     'verify Rebook responsible': function (browser) {
+        browser
+            .clickBySelectorCss('input[value-property-name=id]')
+            .sendKeys('.auto-complete input', 'Mari Alén')
+            .pause(2000)
+            .clickBySelectorXpath('//*[contains(text(),"Mari Alén")]')
+
+            .verify.containsText('//div[text()="Rebook responsible"]/../div/span', "Mari Alén")
     },
 
     'verify Guarantee Valid': function (browser) {
@@ -65,14 +77,15 @@ module.exports = _.assign(presteps, auth, {
             .clickBySelectorXpath('(//radio-item/div/input)[2]')
     },
 
-    'Participation notes': function (browser) {
+    'participation notes': function (browser) {
         browser
             .setValueByXpath('//*[@ngcontrol="packageNotes"]', "test test");
     },
 
-    'click Save': function (browser) {
+     'click Save': function (browser) {
         browser
-            .clickBySelectorXpath('(//*[text()="Save"])[1]')
+            .click('(//*[text()="Save"])[1]')
+            .clickBySelectorXpath('//button[@data-marker="me-confirm__button__button__yes"]')
 
             .waitForElementVisible('//div[@class="toast-content"]', 10000)
             .verify.containsText('//div[@class="toast-title"]', 'Success!')
@@ -87,10 +100,10 @@ module.exports = _.assign(presteps, auth, {
 
     'edit ': function (browser) {
         browser
-            .clickBySelectorXpath('(//option[@value=2])[1]')
+            .clickBySelectorXpath('(//option[@value=1])[1]')
             .clickBySelectorXpath('(//radio-item/div/input)[1]')
             .setValueByXpath('//*[@ngcontrol="packageNotes"]', "test 2 test")
-            .clickBySelectorXpath('(//*[text()="Save"])[1]')
+            .click('(//*[text()="Save"])[1]')
 
             .waitForElementVisible('//div[@class="toast-content"]', 10000)
             .verify.containsText('//div[@class="toast-title"]', 'Success!')
