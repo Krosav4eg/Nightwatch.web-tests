c1c9e8ebddbaf71ee54258e3fc64cbcb6a4ea322
Sergey_Potapov <s.potapov@andersenlab.com>
2017-02-14 19:21:43 +0200

    tests for #26249 #26250 event agenda.run was created

--- e2e/Live Event/table settings/us-14663/#26249 #26250 event agenda.run.js
+++ e2e/Live Event/table settings/us-14663/#26249 #26250 event agenda.run.js
@@ -4,6 +4,7 @@ var auth = require('./../../../presteps/auth.js');
 
 module.exports = _.assign(presteps, auth, {
 
+
     'redirection to agenda': function (browser) {
         browser
             .relUrl('/event/2308/agenda')
@@ -16,7 +17,7 @@ module.exports = _.assign(presteps, auth, {
             .containerCreationForAgenda();
     },
 
-    'add element': function (browser) {
+    'add element button': function (browser) {
         browser
             .clickBySelectorCss('.btn.btn-info.btn-block')
             .useXpath()
@@ -42,7 +43,7 @@ module.exports = _.assign(presteps, auth, {
             .verify.elementPresent('//option[contains(text(),"Lunch Placeholder")]');
     },
 
-    'attach 1-to many placeholder': function (browser) {
+    'select "hosted table placeholder" option.': function (browser) {
         browser
             .clickBySelectorCss('select#agendaElementTypeId')
             .clickBySelectorCss('option[value="23"]')
@@ -52,7 +53,7 @@ module.exports = _.assign(presteps, auth, {
     },
 
 
-    'element requiring table setting assertion': function (browser) {
+    'check fields/elements that appeared below: Agenda Element Type field': function (browser) {
         browser
             .useCss()
             .clickBySelectorCss('select#meetingAllowed')
@@ -68,9 +69,19 @@ module.exports = _.assign(presteps, auth, {
             .clickBySelectorCss('div.col-sm-8.text-center>button.btn.btn-primary')
             .useXpath()
             .verify.elementPresent('//h4[contains(text(),"Add host topics to ")]')
-            .clickBySelectorXpath('(//input[@type="checkbox"])[15]')
-            .clickBySelectorXpath('(//input[@type="checkbox"])[16]')
-            .clickBySelectorXpath('//modal[@class="modal fade in"]//button[@class="btn btn-primary pull-left"]')
+            .clickBySelectorXpath('(//input[@type="checkbox"])[12]')
+            .clickBySelectorXpath('(//input[@type="checkbox"])[13]')
+
+            .clickBySelectorXpath('//modal[@class="modal fade in"]//button[@class="btn btn-primary pull-left"]');
+    },
+
+    'selected hosts are displayed': function (browser) {
+        browser
+            .verify.elementPresent('(//b[contains(text(),"NetApp Denmark ApS")])[1]')
+            .verify.elementPresent('(//modal[@class="modal fade in"]//a[@class="action"])[1]')
+
+            .verify.elementPresent('(//b[contains(text(),"Accenture A/S")])[2]')
+            .verify.elementPresent('(//modal[@class="modal fade in"]//a[@class="action"])[2]')
 
             .setValueByCss('input[ngcontrol="tableSize"]', '2')
             .setValueByCss('input#room', 'My Room')
@@ -82,32 +93,49 @@ module.exports = _.assign(presteps, auth, {
             .useXpath()
             .verify.elementPresent('//h5[contains(text(),"08:55 - 09:25")]')
             .verify.elementPresent('//h5[contains(text(),"Hosted table placeholder")]')
+
             .verify.elementPresent('(//b[contains(text(),"NetApp Denmark ApS")])[1]')
-            .verify.elementPresent('(//b[contains(text(),"Accenture A/S")])[1]');
+            .verify.elementPresent('(//i[@class="fa fa-trash-o delete-element"])[2]')
+
+            .verify.elementPresent('(//b[contains(text(),"Accenture A/S")])[2]')
+            .verify.elementPresent('(//i[@class="fa fa-trash-o delete-element"])[3]');
     },
 
-    'delete 1-to-many-placeholder panel': function (browser) {
+    'click edit agenda button for any hosted table placeholder element in the event agenda.': function (browser) {
         browser
-            .clickBySelectorXpath('//a[3]/i[@class="fa fa-trash-o delete-element"]')
-            .clickBySelectorXpath('//modal[@class="modal fade in"]/div/div/modal-footer/div/button[@data-marker="me-confirm__button__button__yes"]');
+            .useCss()
+            .click('i[class="fa fa-pencil edit-element"]')
+            .pause(3000)
+            .useXpath()
+            .click('(//i[@class="fa fa-trash-o delete-element"])[4]')
+            .clickBySelectorXpath('//modal[@class="modal fade in"]//button[@data-marker="me-confirm__button__button__yes"]')
+
+            .setValueByCss('input#room', 'Room#1')
+
+            .clickBySelectorXpath('//div[@class="col-sm-12 container_btn_group"]/button[2][contains(text(),"Save")]');
+    },
+
+    'delete host': function (browser) {
+        browser
+            .useCss()
+            .clickBySelectorXpath('(//i[@class="fa fa-trash-o delete-element"])[1]')
+
+            .clickBySelectorXpath('//modal[@class="modal fade in"]//button[@data-marker="me-confirm__button__button__yes"]');
     },
 
     '1-to-many-placeholder has been deleted': function (browser) {
         browser
             .useXpath()
             .verify.elementNotPresent('//h5[contains(text(),"08:55 - 09:25")]')
-            .verify.elementNotPresent('//h5[contains(text(),"Hosted table placeholder")]')
-            .verify.elementNotPresent('//i[@class="fa fa-plus"]');
+            .verify.elementNotPresent('//h5[contains(text(),"Hosted table placeholder")]');
     },
 
-    // 'delete container': function (browser) {
-    //     browser
-    //         .useCss()
-    //         .clickBySelectorCss('.fa.fa-trash-o.delete-container')
-    //         .clickBySelectorCss('div.modal-footer>button.btn.btn-success')
-    //         .useXpath()
-    //         .verify.elementNotPresent('//b[contains(text(), "test1")]');
-    // },
-
+    'delete container': function (browser) {
+        browser
+            .clickBySelectorXpath('//a[2]/i[@class="fa fa-trash-o delete-container"]')
+            .clickBySelectorXpath('//modal[@class="modal fade in"]/div/div/modal-footer/div/button[@data-marker="me-confirm__button__button__yes"]')
+            .verify.elementNotPresent('//b[contains(text(), "test1")]')
+            .verify.elementPresent('//div[contains(text(), "Container deleted")]');
+    },
 });
 
